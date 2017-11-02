084826f665c5141eb59479aa25bd9f91fe4e60a2
Thiago Bonotto <thbonotto@gmail.com>
2017-11-02 19:05:05 -0200

    Adicionadas implementações iniciais da interface Web para prover suporte as configurações da rede 802.15.4

---
+++ src/main/java/org/eclipse/kura/micaz/NetworkManageTabUi.java
@@ -0,0 +1,111 @@
+package org.eclipse.kura.micaz;
+
+import org.eclipse.kura.web.client.messages.Messages;
+import org.eclipse.kura.web.client.messages.ValidationMessages;
+import org.eclipse.kura.web.shared.model.GwtGroupedNVPair;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.ListBox;
+import org.gwtbootstrap3.client.ui.TextBox;
+import org.gwtbootstrap3.client.ui.gwt.CellTable;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.RowStyles;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.cellview.client.TextHeader;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.view.client.ListDataProvider;
+
+public class NetworkManageTabUi extends Composite {
+	
+	@UiField
+	TextBox serialInterface;
+	
+	@UiField
+	TextBox panId;
+	
+	@UiField
+    ListBox beaconOrder;
+	
+	@UiField
+    ListBox superframeOrder;
+	
+	@UiField
+	Button networkApply;
+	
+	@UiField
+	Button networkRefresh;
+	
+	
+	@UiField
+	Button nodeUnassociate;
+	
+	@UiField
+	Button nodeRefresh;
+
+	@UiField
+    CellTable<GwtGroupedNVPair> nodesGrid = new CellTable<GwtGroupedNVPair>();
+	private final ListDataProvider<GwtGroupedNVPair> nodesDataProvider = new ListDataProvider<GwtGroupedNVPair>();
+	
+    private static final ValidationMessages msgs = GWT.create(ValidationMessages.class);
+    
+    private static final Messages MSGS = GWT.create(Messages.class);
+	   
+   
+	 private void updateNodesTable(CellTable<GwtGroupedNVPair> grid,
+	            ListDataProvider<GwtGroupedNVPair> dataProvider) {
+		 
+
+	        TextColumn<GwtGroupedNVPair> col1 = new TextColumn<GwtGroupedNVPair>() {
+
+	            @Override
+	            public String getValue(GwtGroupedNVPair object) {
+	                return object.getId();
+	            }
+	        };
+	        col1.setCellStyleNames("status-table-row");
+	        TextHeader id = new TextHeader(MSGS.deviceBndId());
+	        id.setHeaderStyleNames("rowHeader");
+	        grid.addColumn(col1, id);
+
+	        TextColumn<GwtGroupedNVPair> col2 = new TextColumn<GwtGroupedNVPair>() {
+
+	            @Override
+	            public String getValue(GwtGroupedNVPair object) {
+	                return object.getName();
+	            }
+	        };
+	        col2.setCellStyleNames("status-table-row");
+	        TextHeader name = new TextHeader(MSGS.deviceBndName());
+	        name.setHeaderStyleNames("rowHeader");
+	        grid.addColumn(col2, name);
+
+	        TextColumn<GwtGroupedNVPair> col3 = new TextColumn<GwtGroupedNVPair>() {
+
+	            @Override
+	            public String getValue(GwtGroupedNVPair object) {
+	                return msgs.getString(object.getStatus());
+	            }
+	        };
+	        col3.setCellStyleNames("status-table-row");
+	        TextHeader state = new TextHeader(MSGS.deviceBndState());
+	        state.setHeaderStyleNames("rowHeader");
+	        grid.addColumn(col3, state);
+
+	        TextColumn<GwtGroupedNVPair> col4 = new TextColumn<GwtGroupedNVPair>() {
+
+	            @Override
+	            public String getValue(GwtGroupedNVPair object) {
+	                return object.getVersion();
+	            }
+	        };
+	        col4.setCellStyleNames("status-table-row");
+	        TextHeader version = new TextHeader(MSGS.deviceBndVersion());
+	        version.setHeaderStyleNames("rowHeader");
+	        grid.addColumn(col4, version);
+
+	        dataProvider.addDataDisplay(grid);
+
+	 }
+}
+
